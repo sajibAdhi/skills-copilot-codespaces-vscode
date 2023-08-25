b8d9fa57a2dddc57b712337b1ccd85869e61a22c
Sajib Adhikary <60180521+sajibAdhi@users.noreply.github.com>
2023-08-25 18:43:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server
+// 1. Create a web server with the http module
+// 2. Create a server object
+// 3. Listen on port 3000
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
